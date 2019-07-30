0a1591f40fa75c931540ac692f75497f4035a106
Ronald Stephens <rstephens@bluemorpho.cc>
2019-07-30 19:56:02 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -14,10 +14,13 @@ require 'pry'
     }
   }
 
-def remove_strawberry(contacts)
-  contacts.each do |person, contact_details_hash|
-    binding.pry
+  def remove_strawberry(contacts)
+    contacts.each do |person, contact_details_hash|
+      if person == "Freddy Mercury"
+        contact_details_hash.each do |attribute, data|
+          if attribute == :favorite_ice_cream_flavors
+          binding.pry
+        end
+      end
+    end
   end
-
-
-end
